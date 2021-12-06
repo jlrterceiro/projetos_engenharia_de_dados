d98b4fd60058a5ede99d41d3f68e3b4e45871d53
jlrterceiro <jlrterceiro@gmail.com>
2021-12-06 18:29:32 -0300

    Update BitcoinSecondarySort.java

--- apache_hadoop/secondary_sort/BitcoinSecondarySort.java
+++ apache_hadoop/secondary_sort/BitcoinSecondarySort.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
+import java.io.DataInput;
+import java.io.DataOutput;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,41 +17,103 @@ import java.lang.*;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
-
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
 
 public class BitcoinSecondarySort {
 	
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, FloatWritable> {
+	public static class DatePricePair implements WritableComparable<DatePricePair> {
 	
-		private final IntWritable one = new IntWritable(1);
-		private static final String DATE_FORMAT = "yyyy-MM-dd";
+		private Text date = new Text();
+		private IntWritable price = new IntWritable();
+		
+		public Text getDate() { return this.date; }
+		public IntWritable getPrice() { return this.price; }
+		public void setDate(Text date) { this.date = date; }
+		public void setPrice(IntWritable price) { this.price = price; }
 
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			
+		@Override
+		public int compareTo(DatePricePair pair) {
+			int compareValue = this.date.compareTo(pair.getDate());
+			if (compareValue == 0) {
+				compareValue = this.price.compareTo(pair.getPrice());
+				compareValue *= -1;
+			}	
+			return compareValue;
+		}
+		
+		@Override
+		public void readFields(DataInput in) throws IOException {
+         		date = new Text(Integer.toString(in.readInt()));
+         		price = new IntWritable(in.readInt());
+       		}
+		
+		@Override
+		public void write(DataOutput out) throws IOException {
+        		out.writeInt(Integer.parseInt(date.toString()));
+         		out.writeInt(price.get());
+       		}
+	
+	}
+
+	public static class DatePricePartitioner extends Partitioner<DatePricePair, Text> {
+	
+		@Override
+		public int getPartition(DatePricePair pair, Text text, int numberOfPartitions) {
+			return Math.abs(pair.getDate().hashCode()%numberOfPartitions);
+		}
+		
+	}	
+
+	public static class DatePriceGroupingComparator extends WritableComparator {
+		
+		public DatePriceGroupingComparator() {
+			super(DatePricePair.class, true);
+		}
+
+		@Override
+		public int compare(WritableComparable wc1, WritableComparable wc2) {
+			DatePricePair pair = (DatePricePair) wc1;
+			DatePricePair pair2 = (DatePricePair) wc2;
+			return pair.getDate().compareTo(pair2.getDate());
+		}
+
+	}
+
+	
+	public static class TokenizerMapper extends Mapper<Object, Text, DatePricePair, IntWritable> {
+	
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {			   
 			String []words = value.toString().split(",");
 			if (words[0].equals("t")) return;
 			Date date = new java.util.Date( Long.parseLong(words[0]));
-			float h = Float.parseFloat(words[2]);
-			DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+			int price = (int) Float.parseFloat(words[2]);
+			DateFormat df = new SimpleDateFormat("yyyyMMdd");
 			String dateAsString = df.format(date);
-			context.write(new Text(dateAsString), new FloatWritable(h));
-		
+			DatePricePair pair = new DatePricePair();
+			pair.setDate(new Text(dateAsString));
+			pair.setPrice(new IntWritable(price));
+			context.write(pair, new IntWritable(price));
 		}
 	
 	}
 	
-	public static class IntSumReducer extends Reducer<Text, FloatWritable, Text, Text> {
+	public static class IntSumReducer extends Reducer<DatePricePair, IntWritable, Text, Text> {
 		
-		public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+		public void reduce(DatePricePair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			String sum  = "";
 			int counter = 0;
-			for (FloatWritable val : values) {
-				sum += Float.toString(val.get()) + " | ";
+			for (IntWritable val : values) {
+				sum += Integer.toString(val.get()) + " | ";
 				counter++;
 				if (counter==5) break;
 			}
-			context.write(key, new Text(sum));
+			context.write(key.getDate(), new Text(sum));
 		}
+
 	}
 
 	public static void main(String []args) throws Exception {
@@ -59,16 +123,17 @@ public class BitcoinSecondarySort {
 		job.setJarByClass(BitcoinSecondarySort.class);
 		job.setMapperClass(TokenizerMapper.class);
 		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(FloatWritable.class);
+		job.setOutputKeyClass(DatePricePair.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setPartitionerClass(DatePricePartitioner.class);
+		job.setGroupingComparatorClass(DatePriceGroupingComparator.class);
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	
 	}
 
-
 }
-
-
 
